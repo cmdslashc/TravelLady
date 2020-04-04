4e1d6d6218035a9738769db28c3b5a54b8b977f0
Cor <cmdslashc@gmail.com>
2020-04-04 01:05:15 -0400

    New file modifications and ready for section 14

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-conts path = require('path')
+const path = require('path')
 
 module.exports = {
-    entry:'./app/assets/scripts/App.js',
+    entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
-    mode:'development'
+    mode: 'development',
+    watch: true
 }
